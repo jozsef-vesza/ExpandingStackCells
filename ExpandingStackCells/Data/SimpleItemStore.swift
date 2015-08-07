c7e07bbf2eed9fbf75581fb2714f49392e666f73
Jozsef Vesza <jozsef.vesza@outlook.com>
2015-08-07 21:20:16 +0200

    Multiline label example

--- ExpandingStackCells/Data/SimpleItemStore.swift
+++ ExpandingStackCells/Data/SimpleItemStore.swift
@@ -11,7 +11,7 @@ import UIKit
 struct SimpleItemStore {
     static func defaultItems() -> [SimpleItem] {
         return [
-            SimpleItem(title: "Item 0", detail: "Detail 0"),
+            SimpleItem(title: "A really long item title for testing self-sizing mechanism", detail: "A really long detail title to test self-sizing mechanism"),
             SimpleItem(title: "Item 1", detail: "Detail 1"),
             SimpleItem(title: "Item 2", detail: "Detail 2"),
         ]
